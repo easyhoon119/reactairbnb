5d2c5912d2bb79dfcb3566ef8b915f301e4a6289
이지훈 <easyhoon119@github.com>
2021-08-04 10:56:55 +0900

    complete login and makeId

--- src/components/common/LoginModal.js
+++ src/components/common/LoginModal.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { loginBoxAction } from '../../store/actions/loginbox';
 import { IsLoggedAction } from '../../store/actions/isLogged';
 import TextField from '@material-ui/core/TextField';
@@ -10,11 +10,19 @@ function LoginModal() {
 
     const emailInput = useRef();
     const passwordInput = useRef();
+    const nameInput = useRef();
+    const firstNameInput = useRef();
+    const birthInput = useRef();
+    const newEmailInput = useRef();
+    const newPasswordInput = useRef();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [name, setName] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [birth, setBirth] = useState('');
     const [rightMargin, setRightMargin] = useState(15);
     const [whatModal, setWhatModal] = useState('email');
-    const title = whatModal === 'email' ? '로그인 또는 회원가입' : '로그인';
+    const [title, setTitle] = useState('로그인 또는 회원가입');
     const dispatch = useDispatch();
 
     const closeModal = () => {
@@ -29,6 +37,13 @@ function LoginModal() {
             setEmail(emailInput.current.childNodes[1].childNodes[0].value);
             setWhatModal('password');
             setRightMargin(20);
+            setTitle('로그인');
+        }
+
+        else {
+            setEmail(emailInput.current.childNodes[1].childNodes[0].value);
+            setWhatModal('makenewId');
+            setTitle('회원가입 완료하기');
         }
     }
 
@@ -43,13 +58,30 @@ function LoginModal() {
         }
     };
 
+    const returnEmail = () => {
+        setWhatModal('email');
+        setEmail('');
+        setTitle('로그인 또는 회원가입');
+    }
+
+    const goMakeId = () => {
+        setName(nameInput.current.childNodes[1].childNodes[0].value);
+        setFirstName(firstNameInput.current.childNodes[1].childNodes[0].value);
+        setBirth(birthInput.current.childNodes[0].childNodes[0].value);
+        setEmail(newEmailInput.current.childNodes[1].childNodes[0].value);
+        setPassword(newPasswordInput.current.childNodes[1].childNodes[0].value);
+        alert('회원가입이 완료되었습니다. 다시 로그인 해주세요.');
+        console.log(name, firstName, birth, email, password);
+        setWhatModal('email');
+    };
+
     const selectModal = () => {
         if (whatModal === 'email') {
             return (
                 <div className="inner">
                     <p style={{ fontSize: '2vw', fontWeight: '600' }}>에어비엔비에 오신것을 환영합니다.</p>
                     <EmailForm>
-                        <TextField ref={emailInput} id="outlined-basic" color="secondary" fullWidth label="이메일" variant="outlined" placeholder="이메일" />
+                        <TextField ref={emailInput} type="email" id="outlined-basic" color="secondary" fullWidth label="이메일" variant="outlined" placeholder="이메일" />
                         <SubmitButton onClick={goPassword}>계속</SubmitButton>
                         <div style={{ width: '100%', display: 'flex', marginTop: '1.2vw', justifyContent: 'space-between', alignItems: 'center' }}>
                             <div style={{ width: '44%', height: '1px', backgroundColor: '#d5d5d5' }}></div>
@@ -70,14 +102,51 @@ function LoginModal() {
         else if (whatModal === 'password') {
             return (
                 <div className="inner">
-                    <TextField ref={passwordInput} id="outlined-basic" type="password" color="secondary" fullWidth label="비밀번호" variant="outlined" placeholder="비밀번호" />
+                    <TextField ref={passwordInput} id="outlined-basic1" type="password" color="secondary" fullWidth label="비밀번호" variant="outlined" placeholder="비밀번호" />
                     <SubmitButton style={{ marginBottom: '2vw' }} onClick={goLogin}>로그인</SubmitButton>
                     <Link to='#' style={{ fontSize: '1.1vw' }}>비밀번호를 잊으셨나요?</Link>
                 </div>
             );
         }
+
+        else if (whatModal === 'makenewId') {
+            return (
+                <div className="inner">
+                    <TextField ref={nameInput} style={{ marginBottom: '1vw' }} id="outlined-basic2" color="secondary" fullWidth label="이름(예: 길동)" variant="outlined" placeholder="이름(예: 길동)" />
+                    <TextField ref={firstNameInput} style={{ marginBottom: '1vw' }} id="outlined-basic3" color="secondary" fullWidth label="성(예: 홍)" variant="outlined" placeholder="성(예: 홍)" />
+                    <p style={{ color: 'grey', fontSize: '1vw', marginBottom: '1.5vw' }}>정부 발급 신분증에 표시된 이름과 일치하는지 확인하세요.</p>
+                    <TextField ref={birthInput} style={{ marginBottom: '1vw' }} id="outlined-basic4" type="date" color="secondary" fullWidth variant="outlined" />
+                    <p style={{ color: 'grey', fontSize: '1vw', marginBottom: '1.5vw' }}>만 18세 이상의 성인만 회원으로 가입할 수 있습니다. 생일은 에어비엔비의 다른 회원에게 공개되지 않습니다.</p>
+                    <TextField ref={newEmailInput} style={{ marginBottom: '1vw' }} id="outlined-basic5" type="email" color="secondary" fullWidth label="이메일" variant="outlined" value={email} />
+                    <p style={{ color: 'grey', fontSize: '1vw', marginBottom: '1.5vw' }}>예약 확인과 영수증을 이메일로 보내드립니다.</p>
+                    <TextField ref={newPasswordInput} style={{ marginBottom: '1vw' }} id="outlined-basic6" type="password" color="secondary" fullWidth label="비밀번호" variant="outlined" placeholder="비밀번호" />
+                    <p style={{ color: 'grey', fontSize: '1vw' }}>아래의 <span>동의 및 계속하기</span> 버튼을 선택하면, 에어비엔비의 <Link to="#">서비스 약관,</Link> <Link to="#">결제 서비스 약관,</Link> <Link to="#">개인정보 처리 방침,</Link> <Link to="#">차별 금지 정책</Link>에 동의하는 것입니다.</p>
+                    <SubmitButton style={{ marginBottom: '2vw' }} onClick={goMakeId}>동의 및 계속하기</SubmitButton>
+                </div>
+            );
+        }
     }
 
+    const selectWhere = () => {
+        if (whatModal === 'email') {
+            return (
+                <>
+                    <i className="fas fa-times" onClick={closeModal} style={{ fontSize: '1.7vw', fontWeight: '550', marginRight: `${rightMargin}vw` }}></i>
+                </>
+            );
+        }
+
+        else if (whatModal === 'password' || whatModal === 'makenewId') {
+            return (
+                <>
+                    <i className="fas fa-angle-left" onClick={returnEmail} style={{ fontSize: '1.7vw', fontWeight: '550', marginRight: `${rightMargin}vw` }}></i>
+                </>
+            );
+        }
+    };
+
+
+    const goWhere = selectWhere();
     const displayModal = selectModal();
 
     return (
@@ -85,7 +154,7 @@ function LoginModal() {
             <LModal>
                 <RealModal>
                     <div style={{ display: 'flex', cursor: 'pointer', width: '100%', justifyContent: 'left', padding: '2vw', borderBottom: '1px solid #dedede', alignItems: 'center', boxSizing: 'border-box' }}>
-                        <i className="fas fa-times" onClick={closeModal} style={{ fontSize: '1.7vw', fontWeight: '550', marginRight: `${rightMargin}vw` }}></i>
+                        {goWhere}
                         <p style={{ fontSize: '1.4vw', fontWeight: '700' }}>{title}</p>
                     </div>
                     {displayModal}
--- src/components/common/NavBar.js
+++ src/components/common/NavBar.js
@@ -3,6 +3,7 @@ import logo from '../../image/mainlogo.png';
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { loginBoxAction } from '../../store/actions/loginbox';
+import { IsLoggedAction } from '../../store/actions/isLogged';
 
 function NavBar() {
 
@@ -24,16 +25,47 @@ function NavBar() {
         }));
     };
 
+    const goLogout = () => {
+        dispatch(IsLoggedAction({
+            isLogged: false
+        }));
+    };
+
+    const loggedLink = isLogged.isLogged === true ?
+        <>
+            <div style={{ borderBottom: '1px solid #dedede', paddingTop: '1vw' }}>
+                <p>메세지</p>
+                <p>알림</p>
+                <p>여행</p>
+                <p>위시 리스트</p>
+            </div>
+            <div style={{ borderBottom: '1px solid #dedede', paddingTop: '1vw' }}>
+                <p>숙소 관리</p>
+                <p>체험 호스팅하기</p>
+                <p>호스트 추천하기</p>
+                <p>계정</p>
+            </div>
+            <div style={{ paddingBottom: '1vw', marginTop: '0.5vw' }}>
+                <p>도움말</p>
+                <p onClick={goLogout}>로그아웃</p>
+            </div>
+        </>
+        :
+        <>
+            <div style={{ borderBottom: '1px solid #dedede', paddingTop: '1vw' }} onClick={goLoginModal}>
+                <p>로그인</p>
+                <p>회원 가입</p>
+            </div>
+            <div style={{ paddingBottom: '1vw', marginTop: '0.5vw' }}>
+                <p>숙소 호스팅 되기</p>
+                <p>체험 호스팅 하기</p>
+                <p>도움말</p>
+            </div>
+        </>
+        ;
+
     const showLink = isProfile === true ? <ProfileLink>
-        <div style={{ borderBottom: '1px solid #dedede', paddingTop: '1vw' }} onClick={goLoginModal}>
-            <p>로그인</p>
-            <p>회원 가입</p>
-        </div>
-        <div style={{ paddingBottom: '1vw', marginTop: '0.5vw' }}>
-            <p>숙소 호스팅 되기</p>
-            <p>체험 호스팅 하기</p>
-            <p>도움말</p>
-        </div>
+        {loggedLink}
     </ProfileLink> : '';
 
     return (
@@ -48,8 +80,8 @@ function NavBar() {
                     </ul>
                 </div>
                 <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                    <span style={{ marginRight: '18px', fontSize: '14.5px' }}> 호스트 되기</span>
-                    <i className="fas fa-globe" style={{ marginRight: '18px', fontSize: '16px' }}></i>
+                    <span style={{ marginRight: '18px', fontSize: '14.5px', cursor: 'pointer' }}>{isLogged.isLogged === true ? '호스트 모드로 전환' : '호스트 되기'}</span>
+                    <i className="fas fa-globe" style={{ marginRight: '18px', fontSize: '16px', cursor: 'pointer' }}></i>
                     <ProfileSelect onClick={clickProfile}>
                         <i className="fas fa-bars" style={{ color: 'grey', marginRight: '10px' }}></i>
                         <i className="fas fa-user-circle" style={{ color: 'grey', fontSize: '30px' }}></i>
@@ -81,6 +113,7 @@ const Mainlogo = styled.div`
     background-position: center;
     background-size: 100%;
     background-repeat: no-repeat;
+    cursor: pointer;
 `;
 
 const Mainnav = styled.div`
@@ -93,6 +126,10 @@ const Mainnav = styled.div`
     align-items:center;
     font-size:1.25vw;
     font-weight:350;
+
+    li { 
+        cursor : pointer;
+    }
 `;
 
 const ProfileSelect = styled.div`
